236ca9317dee578156c8af26253e1919be0fae34
XLXJXNDRX <ajreverolr@ufpso.edu.co>
2025-03-08 12:43:27 -0500

    Add files via upload

---
+++ quiz/quiz.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class quiz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("profe le voy a decir la verdad no estudie lo suficiente y pues la verdad un quiz no me define pero daña mi autoestima :'( pero bueno lo intente. ");
+        System.out.println("Hola mundooo!!!");
+        System.out.println("la verdad estoy considerando hacer un cambio de carrera :( ");
+
+
+
+
+
+        
+    }
+    }
+    
+
